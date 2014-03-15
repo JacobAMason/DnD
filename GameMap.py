aa59990c94826b6033e559c39aaf5d0cedbf7c18
Alex Dodd <alexdodd@bellsouth.net>
2014-03-15 00:18:09 -0500

    added a coordinate display to the top of screen

--- GameMap.py
+++ GameMap.py
@@ -21,26 +21,33 @@ class PlayerMap(Thread):
         pygame.init()
 
         screen = pygame.display.set_mode(self._area)
+        pygame.display.set_caption('Game Map')
         self.base = pygame.image.load('15x15map.png')
         self.player = pygame.image.load('playstation.png')
         self.other = pygame.image.load('droid.png')
         self.mob = pygame.image.load('creeper.png')
+        TheFont = pygame.font.SysFont(None, 64)
 
         black = 0, 0, 0
+        white = 255, 255, 255
 
         while True:
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
-                    
+            coords = '%s,%s' %(self._ipos,self._jpos)
+            text = TheFont.render(coords,True,white,black)
+
             screen.fill(black)
-            screen.blit(self.base, (-(self._ipos-self._visibility//2)*self._key, -(self._jpos-self._visibility//2)*self._key))
+            screen.blit(self.base, (-(self._ipos-self._visibility//2)*self._key, -(self._jpos-self._visibility//2) * self._key + self._key))
             for person in self._players:
                 screen.blit(self.other,(person))
             for baddie in self._mobs:
                 screen.blit(self.mob,(baddie))
-            screen.blit(self.player,(self._visibility//2 * self._key, self._visibility//2 * self._key))
+            screen.blit(self.player,(self._visibility//2 * self._key, self._visibility//2 * self._key + self._key))
+            pygame.draw.rect(screen,black,(0,0,self._size,self._key))
+            screen.blit(text,(0,0))
             pygame.display.update()
 
 
@@ -55,12 +62,12 @@ class PlayerMap(Thread):
     def players_update(self, position):
         self._player_i = self._visibility//2 + (int(position[0]) - self._ipos)
         self._player_j = self._visibility//2 + (int(position[1]) - self._jpos)
-        self._players.append((self._player_i * self._key, self._player_j * self._key))
+        self._players.append((self._player_i * self._key, self._player_j * self._key + self._key))
 
     def mob_update(self, position):
         self._mob_i = self._visibility//2 + (int(position[0]) - self._ipos)
         self._mob_j = self._visibility//2 + (int(position[1]) - self._jpos)
-        self._mobs.append((self._mob_i * self._key, self._mob_j * self._key))
+        self._mobs.append((self._mob_i * self._key, self._mob_j * self._key + self._key))
 
 if __name__ == '__main__':
     player_position = [1, 3, 0]
